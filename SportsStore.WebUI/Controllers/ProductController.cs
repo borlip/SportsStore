16f675119b5ca07e60308802c64168a5547f155a
Boris Lipschitz <borislipschitz@hotmail.com>
2012-06-09 12:46:48 +1000

    a first draft

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public int PageSize = 2; // We will change this later
+
+        private readonly IProductRepository repository;
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        public ViewResult List(int page = 1)
+        {
+            var viewModel = new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(viewModel);
+        }
+
+    }
+}
