45bad5b077113fdfda60f2c148a937727d123266
Boris Lipschitz <borislipschitz@hotmail.com>
2012-06-09 14:09:27 +1000

    blah

--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -20,11 +20,16 @@ namespace SportsStore.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                null, // we don't need to specify a name
+                "Page{page}",
+                new {Controller = "Product", action = "List"}
+                );
+
+            routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
                 new { controller = "Product", action = "List", id = UrlParameter.Optional } // Parameter defaults
             );
-
         }
 
         protected void Application_Start()
