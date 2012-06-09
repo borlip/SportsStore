41f2226ff2c8aed29be0b96a69bd631d8fef1814
Boris Lipschitz <borislipschitz@hotmail.com>
2012-06-09 16:54:45 +1000

    added model binder + remove item btn

--- SportsStore.WebUI/Binders/CartModelBinder.cs
+++ SportsStore.WebUI/Binders/CartModelBinder.cs
@@ -1,7 +1,24 @@
-﻿namespace SportsStore.WebUI.Binders
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Binders
 {
-    public class CartModelBinder
+    public class CartModelBinder : IModelBinder
     {
-         
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            // get the Cart from the session
+            var cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            // create the Cart if there wasn't one in the session data
+            if (cart == null)
+            {
+                cart = new Cart();
+                controllerContext.HttpContext.Session[sessionKey] = cart;
+            }
+            // return the cart
+            return cart;
+        }
     }
 }
--- SportsStore.WebUI/Global.asax.cs
+++ SportsStore.WebUI/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Binders;
 using SportsStore.WebUI.Infrastructure;
 
 namespace SportsStore.WebUI
@@ -45,6 +47,7 @@ namespace SportsStore.WebUI
             RegisterRoutes(RouteTable.Routes);
 
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
